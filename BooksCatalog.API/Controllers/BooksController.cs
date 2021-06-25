82e054fe3f8d7d1b406a5de8d2e99e2b0709dd6f
Fábio Pereira <fabiopereira.fps@gmail.com>
2021-06-24 22:36:26 -0300

    Add Controllers

---
+++ BooksCatalog.API/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using BooksCatalog.API.Models;
+using BooksCatalog.API.Services.Contracts;
+using BooksCatalog.Domain.Books;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksCatalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksService _booksService;
+
+        public BooksController(IBooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll([FromQuery] Status bookStatus)
+        {
+            var books = _booksService.GetAll(bookStatus);
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddBookRequest request)
+        {
+            _booksService.Add(request);
+            return Ok();
+        }
+    }
+}
---
+++ BooksCatalog.API/Models/AddBookRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BooksCatalog.API.Models
+{
+    public class AddBookRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
