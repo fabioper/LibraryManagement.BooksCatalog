e441533e25a7c10b258386b234a19cfc720834b0
Fábio Pereira <fabiopereira.fps@gmail.com>
2021-06-25 01:16:06 -0300

    Add Book Created event

--- BooksCatalog.API/Services/BooksService.cs
+++ BooksCatalog.API/Services/BooksService.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections.Generic;
+using BooksCatalog.API.Events.Messages;
 using BooksCatalog.API.Models;
 using BooksCatalog.API.Services.Contracts;
 using BooksCatalog.Domain.Books;
 using BooksCatalog.Domain.Interfaces;
+using BooksCatalog.Infra.Messaging.Contracts;
+using Microsoft.Extensions.Logging;
 
 namespace BooksCatalog.API.Services
 {
     public class BooksService : IBooksService
     {
         private readonly IBookRepository _bookRepository;
+        private readonly IServiceBus _serviceBus;
+        private readonly ILogger<BooksService> _logger;
 
-        public BooksService(IBookRepository bookRepository)
+        public BooksService(IBookRepository bookRepository, IServiceBus serviceBus, ILogger<BooksService> logger)
         {
             _bookRepository = bookRepository;
+            _serviceBus = serviceBus;
+            _logger = logger;
         }
 
         public IEnumerable<Book> GetAll(bool? isAvailable)
@@ -27,6 +34,9 @@ namespace BooksCatalog.API.Services
             var book = new Book(request.Title, request.Description, request.Author);
             _bookRepository.Add(book);
             _bookRepository.SaveChanges();
+            
+            _serviceBus.Publish(new BookCreated(book.Id));
+            _logger.LogInformation($"Book {book.Id} created");
         }
     }
 }
