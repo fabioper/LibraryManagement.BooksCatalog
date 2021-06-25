0b7adcbf5b595372af06f9b6ef1ed8bf62f2b160
Fábio Pereira <fabiopereira.fps@gmail.com>
2021-06-24 22:36:02 -0300

    Create BooksService

---
+++ BooksCatalog.API/Services/BooksService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using BooksCatalog.API.Models;
+using BooksCatalog.API.Services.Contracts;
+using BooksCatalog.Domain.Books;
+using BooksCatalog.Domain.Interfaces;
+
+namespace BooksCatalog.API.Services
+{
+    public class BooksService : IBooksService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public IEnumerable<Book> GetAll(Status bookStatus)
+        {
+            return _bookRepository.GetByStatus(bookStatus);
+        }
+        
+        public IEnumerable<Book> GetAll()
+        {
+            return _bookRepository.GetAll();
+        }
+
+        public void Add(AddBookRequest request)
+        {
+            var book = new Book(request.Title, request.Description, request.Author);
+            _bookRepository.Add(book);
+            _bookRepository.SaveChanges();
+        }
+    }
+}
